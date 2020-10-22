d8f8eb2d23cecc3904d30d4ab8572f151960a002
Nereid3611 <71778576+Nereid3611@users.noreply.github.com>
2020-10-22 17:25:15 +0900

    Initial commit

---
+++ Assets/ItenGenerator.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class ItenGenerator : MonoBehaviour
+{
+    public GameObject CarTypeA;
+    public GameObject PointCoin;
+    public GameObject ObstacleCone;
+
+    private int StartPos = 80;
+    private int GoalPos = 360;
+
+    private float PopRange = 3.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = StartPos; i<GoalPos ; i+=15)
+        {
+            int num　= Random.Range(1, 11);
+            //コーンＰＯＰ↓
+            if (num <= 2)
+            {
+                for(float j = -1; j<=1; j+=0.4f)
+                {
+                    GameObject cone = Instantiate(ObstacleCone);
+                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
+                }
+            }
+            //ココマデ↑
+            else
+            {
+                for(int j = -1; j<=1; j++)
+                {
+                    int item = Random.Range(1, 11);
+                    int SetPositionZ = Random.Range(-5, 6);
+                    //コイン精製
+                    if (1 <= item && item <= 6)
+                    {
+                        GameObject coin = Instantiate(PointCoin);
+                        coin.transform.position = new Vector3(PopRange * j, coin.transform.position.y, i + SetPositionZ);
+                    }
+                    //車精製
+                    else if (7 <= item && item <= 9)
+                    {
+                        GameObject car = Instantiate(CarTypeA);
+                        car.transform.position = new Vector3(PopRange * j, car.transform.position.y, i + SetPositionZ);
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnityChanController : MonoBehaviour
+{
+    private Animator myAnimator;
+    private Rigidbody myRigdbody;
+    private GameObject EndGameText;
+    private GameObject ScoreText;
+
+    private int Score = 0 ;
+
+    private float VelZ = 16f;
+    private float VelX = 10f;
+    private float VelY = 10f;
+    private float MoveRange = 3.4f;
+    private float EndStop = 0.99f;
+
+    private bool isEnd = false;
+    private bool LB = false;
+    private bool RB = false;
+    private bool JB = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.myAnimator = GetComponent<Animator>();
+        this.myAnimator.SetFloat("Speed", 1);
+
+        this.myRigdbody = GetComponent<Rigidbody>();
+        this.EndGameText = GameObject.Find("ResultUI");
+        this.ScoreText = GameObject.Find("ScoreUI");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キーコントロール↓
+        float LRmoveSpeed = 0;
+        float JumpSpeed = 0;
+                                            　　　　　　　　　　　　　　　　 //左端の座標
+        if((Input.GetKey(KeyCode.LeftArrow) || this.LB) && -this.MoveRange < this.transform.position.x)
+        {
+            LRmoveSpeed = -this.VelX;
+
+        }
+        else if ((Input.GetKey(KeyCode.RightArrow) || this.RB) &&  this.transform.position.x < this.MoveRange)
+        {
+            LRmoveSpeed = this.VelX;
+
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space) || this.JB) && this.transform.position.y < 0.5f)
+        {
+            this.myAnimator.SetBool("Jump", true);
+            JumpSpeed = VelY;
+
+        }
+        else 
+        {
+            JumpSpeed = this.myRigdbody.velocity.y;
+
+        }
+        if(this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+
+        }
+        this.myRigdbody.velocity = new Vector3(LRmoveSpeed, JumpSpeed, VelZ);
+        //キーコントロール↑
+
+        if (this.isEnd)
+        {
+            this.VelX *= EndStop;
+            this.VelY *= EndStop;
+            this.VelZ *= EndStop;
+            this.myAnimator.speed *= this.EndStop;
+        }
+
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag =="ConeTag")
+        {
+            this.isEnd = true;
+            this.EndGameText.GetComponent<Text>().text = "Game Over";
+
+
+        }
+        if (other.gameObject.tag == "GoalTag")
+        {
+            this.isEnd = true;
+            this.EndGameText.GetComponent<Text>().text = "CLEAR";
+        }
+        if (other.gameObject.tag == "CoinTag")
+        {
+            GetComponent<ParticleSystem>().Play();
+            Destroy(other.gameObject);
+            this.Score += 10;
+            this.ScoreText.GetComponent<Text>().text = "Score" + this.Score + "pt";
+
+        }
+    }
+    //タッチボタン↓
+    public void GetMyJumpButtonDown()
+    {
+        this.JB = true;
+
+    }
+    public void GetMyJumpButtonUp()
+    {
+        this.JB = false;
+
+    }
+    public void GetMyLeftButtonDown()
+    {
+        this.LB = true;
+
+    }
+    public void GetMyLeftButtonUp()
+    {
+        this.LB = false;
+
+    }
+    public void GetMyRightButtonDown()
+    {
+        this.RB = true;
+
+    }
+    public void GetMyRightButtonUp()
+    {
+        this.RB = false;
+
+    }
+}//タッチボタン↑
+
